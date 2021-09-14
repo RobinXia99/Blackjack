a3995b28030807501c154e7367199443db155ab7
Robin Xia <robin.xia@iths.se>
2021-09-14 23:32:50 +0200

    Rules added

--- app/src/main/java/com/example/projectblackjack/Menu.kt
+++ app/src/main/java/com/example/projectblackjack/Menu.kt
@@ -1,11 +1,55 @@
 package com.example.projectblackjack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 
 class Menu : AppCompatActivity() {
+
+
+    lateinit var rulesDialog: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val rulesBtn = findViewById<Button>(R.id.rulesBtn)
+        val playBtn = findViewById<Button>(R.id.playBtn)
+        val dialogBtn = findViewById<Button>(R.id.dialogBtn)
+        rulesDialog = findViewById(R.id.rulesDialog)
+
+        playBtn.setOnClickListener() {
+            startGame()
+        }
+
+        rulesBtn.setOnClickListener() {
+            rulesDialog.setVisibility(View.VISIBLE)
+            dialogBtn.setVisibility(View.VISIBLE)
+            rulesBtn.setVisibility(View.GONE)
+            playBtn.setVisibility(View.GONE)
+
+        }
+        dialogBtn.setOnClickListener() {
+            rulesDialog.setVisibility(View.GONE)
+            dialogBtn.setVisibility(View.GONE)
+            rulesBtn.setVisibility(View.VISIBLE)
+            playBtn.setVisibility(View.VISIBLE)
+
+
+        }
+
+
     }
+
+    private fun startGame () {
+        val intent = Intent(this, Game::class.java)
+        startActivity(intent)
+    }
+
 }
